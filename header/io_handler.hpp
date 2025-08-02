1ce7a2653a88dc4e102229b7327eae12b9c2d693
YasamanAmoujafary <yasamanamoujafary@gmail.com>
2025-08-02 20:25:10 +0330

    cleaning the code

--- header/io_handler.hpp
+++ header/io_handler.hpp
@@ -7,14 +7,18 @@ class IOHandler
 {
 public:
     IOHandler(const string &user_file, const string &movie_file);
-    vector<Movie*> mapTitlesToMovies(const vector<string>& titles) ;
+
     void runCommandLoop();
     void handleCommand(const string &line);
-    vector<string> parseCommand(const string &inputLine);
-    vector<pair<Movie*, float>> handleGenreRecommandation(const vector<string> &parts);
-    vector<Movie*> handleCastRecommandation(const vector<string> &parts);
     void printGenreRecommandedMovies(vector<pair<Movie*, float>>& scoredMovies);
     void printCastRecommandedMovies(vector<Movie*>& castMovies);
+
+    vector<string> parseCommand(const string &inputLine);
+
+    vector<pair<Movie*, float>> handleGenreRecommandation(const vector<string> &parts);
+    
+    vector<Movie*> mapTitlesToMovies(const vector<string>& titles) ;
+    vector<Movie*> handleCastRecommandation(const vector<string> &parts);
 
 private:
     MovieRecommender *movieRecommender;
--- header/registered_user.hpp
+++ header/registered_user.hpp
@@ -7,19 +7,22 @@ class RegisteredUser : public User
 {
 public:
     RegisteredUser(string _name,vector<Movie*> _watched,const vector<string> _ratings);
-    vector<pair<Movie*, float>> recommendByGenre( const string& genre, const vector<Movie*>& movies, const vector<RegisteredUser*>& users);
-    vector<Movie*> recommendByCast(const string& cast, const vector<Movie*>& movies);
     string get_name() const;
-    vector<Movie*> getMovies(){return watchedMovies;}
+    string findFavoriteGenre();
+
     vector<string> getRatings(){return ratings;}
+    vector<Movie*> recommendByCast(const string& cast, const vector<Movie*>& movies);
+    vector<Movie*> getMovies(){return watchedMovies;}
     vector<Movie*> getMovieOfOneGenre(const string &genre, const vector<Movie*>& movies);
     vector<Movie*> checkUserWatchedMovies(const vector<Movie*> &movies);
+    vector<Movie*> filterUnwatchedMatchingCastAndGenre(const string& cast,const string& genre,const vector<Movie*>& movies);
+    vector<pair<Movie*, float>> recommendByGenre( const string& genre, const vector<Movie*>& movies, const vector<RegisteredUser*>& users);
+    
     bool checkMovieExist(const Movie *movie);
+    
     float calculateMovieScore(const string &genre,const Movie* movie, const vector<RegisteredUser*>& users);
     int calculateDirectorEffect(const Movie* movie);
     int calculateCastEffect(const Movie* movie);
-    string findFavoriteGenre();
-    vector<Movie*> filterUnwatchedMatchingCastAndGenre(const string& cast,const string& genre,const vector<Movie*>& movies);
 
 private:
     string name;
